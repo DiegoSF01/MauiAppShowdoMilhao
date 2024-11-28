6b3046728f6e74927835f3dda8f5b1d5e0e9be00
DiegoSF01 <diegosouzafurlanetto@gmail.com>
2024-11-28 15:56:31 -0300

    Show do milhão feito com Maui

---
+++ MauiAppShowDoMilhao/MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using Plugin.Maui.Audio;
+
+namespace MauiAppShowDoMilhao
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+            // https://stackoverflow.com/questions/72463558/how-to-play-an-audio-file-net-maui
+            builder.Services.AddSingleton(AudioManager.Current);
+            
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
